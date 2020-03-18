5f12a62b6498175bf0da85f458315d84ca5494fe
Thevyansh <thevyansh@gmail.com>
2020-03-18 14:10:31 +0530

    Fixed imports

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-const {forecast} = require('./utils/forecast');
-const {geocode} = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
 
 
 const app = express();
